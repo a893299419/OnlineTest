a689fcbb85041aea322e31716222127762ace508
jinyi <925023204@qq.com>
2019-12-09 22:45:59 +0800

    提交代码

--- onlineTest/src/main/java/com/ot/OnlineTestApplication.java
+++ onlineTest/src/main/java/com/ot/OnlineTestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.winterchen.dao")
+@MapperScan("com.ot.dao")
 public class OnlineTestApplication {
 
 	public static void main(String[] args) {
